f17105450e0d6b5b05d3c9fee351487ac9593628
Laboratório FIAP <labsfiap@fiap.com.br>
2025-03-31 20:47:26 -0300

    att

--- src/br/fiap/util/Util.java
+++ src/br/fiap/util/Util.java
@@ -12,6 +12,8 @@ import static javax.swing.JOptionPane.showMessageDialog;
 public class Util {
     private Fornecedor[] fornecedor = new Fornecedor[5];
     private Produto[] produto = new Produto[2 * fornecedor.length];
+    private int idxFornecedor = 0;
+    private int idxProduto = 0;
 
 }
 
@@ -20,6 +22,7 @@ private void menuPrincipal() {
     String menu = "1. Cadastrar Produto\n2. Pesquisar produto por nome\n3. Pesquisar fornecedor por CNPJ\n4. Finalizar";
 
     while (true) {
+
         opcao = parseInt(showInputDialog(menu));
         if (opcao == 4) {
             return;
@@ -38,19 +41,37 @@ private void menuPrincipal() {
             default:
                 showMessageDialog(null, "Opção inválida");
         }
-
     }
 }
 
 public void cadastrarProduto() {
+    String nome;
+    int qtdEstoque;
+    double valorUnitario;
+    Fornecedor fornecedor = pesquisarFornecedor();
+    if (fornecedor == null) {
+        cadastrarFornecedor();
+    }
+}
 
+public void cadastrarFornecedor() {
+    long cnpj = parseLong(showInputDialog("CNPJ do fornnecedor"));
+    String nome = showInputDialog("Fornecedor")
+    fornecedor[idxFornecedor] = new Fornecedor(nome, cnpj);
+    idxFornecedor++;
 }
 
 public Fornecedor pesquisarFornecedor() {
+    long cnpj = parseLong(showInputDialog("CNPJ do fornecedor"));
 
+    for (int i = 0; i < idxFornecedor; i++) {
+        if (fornecedor[i].getCNPJ() == cnpj) {
+            return fornecedor[i];
+        }
+    }
 }
 
-public void pesquisarProduto () {
+public void pesquisarProduto() {
 
 }
 
